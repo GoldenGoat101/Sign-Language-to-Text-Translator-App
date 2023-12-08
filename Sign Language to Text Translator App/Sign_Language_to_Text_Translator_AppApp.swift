ca51058ddd287cc66818bfe2ec1ea0fb24bf2d8e
GoldenGoat101 <aaditmaheshwari1@gmail.com>
2023-12-08 10:46:33 +0530

    Initial Commit

---
+++ Sign Language to Text Translator App/Sign_Language_to_Text_Translator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_Language_to_Text_Translator_AppApp.swift
+//  Sign Language to Text Translator App
+//
+//  Created by Aadit Maheshwari  on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Sign_Language_to_Text_Translator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sign Language to Text Translator AppUITests/Sign_Language_to_Text_Translator_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sign_Language_to_Text_Translator_AppUITestsLaunchTests.swift
+//  Sign Language to Text Translator AppUITests
+//
+//  Created by Aadit Maheshwari  on 08/12/23.
+//
+
+import XCTest
+
+final class Sign_Language_to_Text_Translator_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
